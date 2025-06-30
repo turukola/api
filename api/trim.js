7eed64bd74110400ebd26c101b9358359d770042
turukola <babonekukang12@gmail.com>
2025-07-01 00:55:41 +0700

    trim.js

--- api/trim.js
+++ api/trim.js
@@ -1,6 +1,16 @@
-
 export default async function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).end("Method Not Allowed");
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.status(204).end(); // No Content
+    return;
+  }
+
+  if (req.method !== "POST") {
+    res.status(405).end("Method Not Allowed");
+    return;
+  }
 
   const token = req.headers.authorization || "";
 
